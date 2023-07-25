81e4dc746af6a4379dc50f730c62944bd91d93dd
Adrian Dziekonski <adrian.dziekonski@onet.pl>
2023-07-25 14:51:49 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RutekApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"g:\Praca\IT\C#\RutekApp\liczby_nieposortowane.txt";
+            string pathAfterWork = @"g:\Praca\IT\C#\RutekApp\liczby_posortowane.txt";
+            
+            
+            string nieposortowaneString = File.ReadAllText(path);
+            Console.WriteLine("Nieposortowane liczby: "+ nieposortowaneString);
+
+            List<string> splitString;
+
+            splitString=RutekApp.Split(nieposortowaneString);
+
+            Console.WriteLine("podzielony string: ");
+            RutekApp.wyświetlListe(splitString);
+
+            //List<int> nieposortowaneInty;
+            Console.WriteLine("stringi zamienienione w inty: ");
+            List<int> nieposortowaneInty =splitString.ConvertAll(s => Int32.Parse(s));
+            
+
+            var sum = nieposortowaneInty[0] + 2;        //sprawdzenie czy przekonwertowało na int
+            Console.WriteLine("suma: "+sum);
+
+            
+            Console.WriteLine("Quick sort: ");
+            RutekApp.quickSort(nieposortowaneInty,0,nieposortowaneInty.Count-1);
+            RutekApp.wyświetlListeInt(nieposortowaneInty);
+
+
+            RutekApp.zapiszListeDoPliku(nieposortowaneInty);
+
+
+            
+        }
+    }
+}
---
+++ RutekApp.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.VisualBasic;
+
+namespace RutekApp
+{
+    class RutekApp
+    {
+
+        public static List<string> Split(string input)
+        {
+            var result = new List<string>();
+            var index = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ',')
+                {
+                    var word = input.Substring(index, i - index);
+                    result.Add(word);
+                    index = i + 1;
+                }
+                else if (i == input.Length - 1)
+                {
+                    var word = input.Substring(index, input.Length - index);
+                    result.Add(word);
+                }
+
+            }
+
+
+            return result;
+        }
+
+        public List<string> WriteTheSame(string input)
+        {
+            var resultat = new List<string>();
+            var indexik = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == input[i++])
+                {
+                    var same = input.Substring(indexik, i - indexik);
+                    resultat.Add(same);
+                    indexik = i + 1;    
+                }
+                else if (i == input.Length - 1)
+                {
+                    var word = input.Substring(indexik, input.Length - indexik);
+                    resultat.Add(word);
+                }
+            }
+            return resultat;
+        }
+        public static void wyświetlListe(List<string> list)
+        {
+            int l=list.Count;
+            for (int i = 0; i < l; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+
+        public static void wyświetlListeInt(List<int> list)
+        {
+            int l = list.Count;
+            for (int i = 0; i < l; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+
+       
+
+        public static void quickSort(List<int> list, int left, int right)
+        {
+            var i = left;
+            var j = right;
+            var pivot = list[(left + right) / 2];
+            while (i < j)
+            {
+                while (list[i] < pivot) i++;
+                while (list[j] > pivot) j--;
+                if (i <= j)
+                {
+                    // swap
+                    var tmp = list[i];
+                    list[i++] = list[j];  
+                    list[j--] = tmp;
+                }
+            }
+            if (left < j) quickSort(list, left, j);
+            if (i < right) quickSort(list, i, right);
+            //wyświetlListeInt(list);
+        }
+
+        public static void zapiszListeDoPliku(List<int>list)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(@"g:\Praca\IT\C#\RutekApp\liczby_posortowane.txt");
+            for (int i = 0; i < list.Count; i++)
+            {
+                file.WriteLine(list[i]);
+            }
+            file.Close();
+        }
+    }
+}
+
